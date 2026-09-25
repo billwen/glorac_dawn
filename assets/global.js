01215ac0d66f271609dbf2be7eddb3d5797c1c09
Gang Wen <gang.wen@ingka.ikea.com>
2026-09-25 22:47:34 +0200

    try12

--- assets/global.js
+++ assets/global.js
@@ -435,7 +435,7 @@ class MenuDrawer extends HTMLElement {
       summary.addEventListener('click', this.onSummaryClick.bind(this))
     );
     this.querySelectorAll(
-      'button:not(.localization-selector):not(.country-selector__close-button):not(.country-filter__reset-button)'
+      'button:not(.localization-selector):not(.country-selector__close-button):not(.country-filter__reset-button):not(.menu-drawer__reference-close)'
     ).forEach((button) => button.addEventListener('click', this.onCloseButtonClick.bind(this)));
   }
 
@@ -558,6 +558,9 @@ customElements.define('menu-drawer', MenuDrawer);
 class HeaderDrawer extends MenuDrawer {
   constructor() {
     super();
+    this.querySelector('.menu-drawer__reference-close')?.addEventListener('click', (event) => {
+      this.closeMenuDrawer(event, this.mainDetailsToggle.querySelector('summary'));
+    });
   }
 
   openMenuDrawer(summaryElement) {
